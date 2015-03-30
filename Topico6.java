b88296f37cc6ba79bab5c265216672b387b4236b
ChristianARamos <christian.a.ramos@gmail.com>
2015-03-30 17:26:00 -0300

    Alteração3

--- Topico6.java
+++ Topico6.java
@@ -53,7 +53,7 @@ public class Topico6 {
             if(result == false){
                 System.out.println("A Pasta não existe. Criando pasta Programacao...");
                 pasta.criarPasta("\\POOII\\Programacao");
-                result = pasta.existePasta("\\", "POOII\\Programacao");
+                result = pasta.existePasta("\\POOII\\", "Programacao");
                     if (result == true){
                         System.out.println("Pasta criada...");
                     } else{System.out.println("A Pasta não foi criada.");                        }
@@ -61,21 +61,69 @@ public class Topico6 {
                 System.out.println("A pasta Programacao já existe.");
                 }
             //================================================================
-                        result = pasta.existePasta("\\POOII\\", "OrientacaoObjeto");
+            result = pasta.existePasta("\\POOII\\Programacao\\", "JAVA");
+            if(result == false){
+                System.out.println("A Pasta não existe. Criando pasta JAVA...");
+                pasta.criarPasta("\\POOII\\Programacao\\JAVA");
+                result = pasta.existePasta("\\POOII\\Programacao\\", "JAVA");
+                    if (result == true){
+                        System.out.println("Pasta criada...");
+                    } else{System.out.println("A Pasta não foi criada.");                        }
+            } else{
+                System.out.println("A pasta JAVA já existe.");
+                }
+            //================================================================
+            result = pasta.existePasta("\\POOII\\Programacao\\JAVA\\", "2015");
+            if(result == false){
+                System.out.println("A Pasta não existe. Criando pasta 2015...");
+                pasta.criarPasta("\\POOII\\Programacao\\JAVA\\2015");
+                result = pasta.existePasta("\\POOII\\Programacao\\JAVA\\", "2015");
+                    if (result == true){
+                        System.out.println("Pasta criada...");
+                    } else{System.out.println("A Pasta não foi criada.");                        }
+            } else{
+                System.out.println("A pasta 2015 já existe.");
+                }
+            //================================================================
+            result = pasta.existePasta("\\POOII\\", "OrientacaoObjeto");
             if(result == false){
                 System.out.println("A Pasta não existe. Criando pasta OrientacaoObjeto...");
                 pasta.criarPasta("\\POOII\\OrientacaoObjeto");
-                result = pasta.existePasta("\\", "POOII\\OrientacaoObjeto");
+                result = pasta.existePasta("\\POOII\\", "OrientacaoObjeto");
                     if (result == true){
                         System.out.println("Pasta criada...");
                     } else{System.out.println("A Pasta não foi criada.");                        }
             } else{
                 System.out.println("A pasta OrientacaoObjeto já existe.");
                 }
+            //================================================================
+            result = pasta.existePasta("\\POOII\\OrientacaoObjeto\\", "Turma");
+            if(result == false){
+                System.out.println("A Pasta não existe. Criando pasta Turma...");
+                pasta.criarPasta("\\POOII\\OrientacaoObjeto\\Turma");
+                result = pasta.existePasta("\\POOII\\OrientacaoObjeto\\", "Turma");
+                    if (result == true){
+                        System.out.println("Pasta criada...");
+                    } else{System.out.println("A Pasta não foi criada.");                        }
+            } else{
+                System.out.println("A pasta Turma já existe.");
+                }
+            //================================================================
+            result = pasta.existePasta("\\POOII\\OrientacaoObjeto\\Turma\\", "Montenegro");
+            if(result == false){
+                System.out.println("A Pasta não existe. Criando pasta Montenegro...");
+                pasta.criarPasta("\\POOII\\OrientacaoObjeto\\Turma\\Montenegro");
+                result = pasta.existePasta("\\POOII\\OrientacaoObjeto\\Turma\\", "Montenegro");
+                    if (result == true){
+                        System.out.println("Pasta criada...");
+                    } else{System.out.println("A Pasta não foi criada.");                        }
+            } else{
+                System.out.println("A pasta Montenegro já existe.");
+                }
+            
             
         }catch(Exception e){
             System.out.println("Ocorreu um erro. A pasta não foi criada.");
         }
-        
     }
 }
